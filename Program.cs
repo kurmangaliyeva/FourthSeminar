86e88dc7cff4a3627c50d37aa977f915333d951f
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-08-10 00:21:20 +0600

    Second Task solved

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace FourthSeminar
             // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
             // [345, 897, 568, 234] -> 2
 
-
-            Console.WriteLine($"Программа выдает количество чётных чисел в массиве:\n");
+            /*
+            Console.WriteLine($"Программа выдает количество чётных чисел в массиве ");
             int[] numbers = new int[10];
 
             void FillArray(int[] array, int min, int max){
@@ -42,7 +42,39 @@ namespace FourthSeminar
 
             int quantity = QuantityOfPositive(numbers);
             Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
+            */
 
+            // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+            // [3, 7, 23, 12] -> 19
+            // [-4, -6, 89, 6] -> 0
+
+
+            Console.WriteLine($"Программа считает сумму элементов, стоящих на нечётных позициях ");
+            Console.Write($"Введите количество элементов массива ");
+            int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+            int RandomNumbers(int numberElements, int min, int max){
+            int[] randomNumbers = new int[numberElements];
+            int sumElements = 0;
+            Console.Write("Получившийся массив: ");
+
+                for (int i = 0; i <randomNumbers.Length; i++ ){
+                randomNumbers[i] = new Random().Next(min, max);
+
+                Console.Write(randomNumbers[i] + " ");
+
+                if (i % 2 != 1)
+                {
+                    sumElements = sumElements + randomNumbers[i];
+                }
+                }
+
+            return sumElements;
+            }
+
+            int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+            Console.WriteLine();
+            Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {randomNumbers}");
 
 
 
